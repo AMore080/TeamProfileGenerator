3c1fe97848089283d123bd61f00c02ff78ffd5d7
AMore080 <alexmore080@gmail.com>
2022-09-13 18:04:40 -0500

    created Employee test constructor

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../public/scripts/Employee');
+const employee = new Employee("Alex","1234","alexmore050@gmail.com")
+
+test("This should return the values of the Employee Constructor", () => {
+    expect(employee.name).toEqual("Alex");
+    expect(employee.id).toEqual("1234");
+    expect(employee.email).toEqual("alexmore050@gmail.com");
+})
+
+test("Getting name from getName() Method", () => {
+    expect(employee.getName()).toEqual('Alex');
+})
+
+test("Getting ID from getID() method", () => {
+    expect(employee.getID()).toEqual("1234");
+})
+
+test("Getting ID from getEmail() method", () => {
+    expect(employee.getEmail()).toEqual("alexmore050@gmail.com");
+})
